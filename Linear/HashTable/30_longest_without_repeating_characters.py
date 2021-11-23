4ef4ab5321949dd9603eaabf805612881faddf79
jinsim <simsime@korea.ac.kr>
2021-11-23 17:56:13 +0900

    31 추가

--- Linear/HashTable/30_longest_without_repeating_characters.py
+++ Linear/HashTable/30_longest_without_repeating_characters.py
@@ -2,12 +2,14 @@
 # 중복 문자가 없는 가장 긴 부분 문자열(substring)의 길이를 리턴하라. 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # 해시테이블
         used = {}
         max_length = start = 0
         # s를 enumerate 돌린다.
         for index, char in enumerate(s):
             # 이미 등장했던 문자라면, start의 위치를 갱신한다.
-            if char in used:
+            # 이때 슬라이딩 윈도우 시작점 전에 있는 문자는 used에 있어도 무시해야한다.
+            if char in used and start <= used[char]:
                 start = used[char] + 1
             # 등장하지 않았던 문자라면 최대 substring 길이를 갱신한다.
             else:
@@ -15,3 +17,4 @@ class Solution:
             # 현재 문자의 위치를 삽입한다.
             used[char] = index
         return max_length
+ 

5207959eadac57e98344479c3a745c08c2605358
jinsim <simsime@korea.ac.kr>
2022-06-25 18:47:13 +0900

    32 추가

---
+++ Nonlinear/Graph/32_number_of_islands.py
@@ -0,0 +1,26 @@
+class Solution:
+    grid: List[List[str]] # 매개변수에서 제거하기 위해 클래스를 멤버 변수로 선언
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        def dfs(self, grid: List[List[str]], i: int, j: int): # self 제거를 위해 중첩함수로 선언했다.
+            # 더 이상 육지가 아닌 경우 종료
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+
+            grid[i][j] = '0'
+
+            # 동서남북 탐색
+            self.dfs(grid, i+1, j)
+            self.dfs(grid, i-1, j)
+            self.dfs(grid, i, j+1)
+            self.dfs(grid, i, j-1)
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    # 모든 육지 탐색 후 카운트 1 증가
+                    count += 1
+        return count

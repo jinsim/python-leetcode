7f0517d377c12a738ffe47955bc4d4f0a21ca202
jinsim <simsime@korea.ac.kr>
2022-06-30 09:45:03 +0900

    37 추가

--- Nonlinear/Graph/35_combinations.py
+++ Nonlinear/Graph/35_combinations.py
@@ -12,13 +12,15 @@ Output:
   [1,4],
 ]
 """
-
+import itertools
 class Solution:
-    """
+    """ 1) DFS로 k개 조합 생성
     k개의 조합만을 생성해야한다는 제약 조건이 추가되었고, 조합의 특성 상 본인 이전의 모든 숫자를 제외해야하므로 n과 k를 추가하였다.
     dfs에 k가 0이되면 백트래킹이 되도록 설정하고, 자신 이전의 값은 무시하면서 각 숫자들을 elements에 넣어 재귀 호출하였다.
+
+    k와 n을 뒤집어서 k-1을 재귀호출하는 형태로 하면 탐색 범위를 훨씬 좁혀나갈 수 있는데, 알고리즘을 직관적으로 이해하기가 너무 어려우므로, 이정도로 마무리하는 것이 좋다.
     """
-    def combine(self, n: int, k: int) -> List[List[int]]:
+    def combine_1(self, n: int, k: int) -> List[List[int]]:
         results = []
         
         def dfs(elements, start: int, k: int):
@@ -34,3 +36,10 @@ class Solution:
         
         dfs([], 1, k)
         return results
+
+    """ 2) itertools를 이용한 풀이
+    permutations()처럼, combinations()를 사용하면 된다.
+    순열과 달리 조합의 경우 DFS와 모듈의 성능 차이가 상당히 큰 편이다.
+    """
+    def combine_2(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(range(1, n + 1), k))
---
+++ Nonlinear/Graph/37_subsets.py
@@ -0,0 +1,25 @@
+""" 37. 부분 집합 (78) - 355
+모든 부분 집합을 리턴하라.
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+"""
+class Solution:
+    """ 1) 트리의 모든 DFS 결과
+    경로 path를 만들어 나가면서 인덱스를 1씩 증가하는 형태로 깊이 탐색한다. 별도의 종료 조건 없이 탐색이 끝나면 저절로 함수가 종료되게 한다.
+    
+    부분 집합은 모든 탐색의 경로가 결국 정답이 되므로, 같이 탐색할 때마다 매번 결과를 추가하면 된다.
+    """
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        
+        def dfs(index, path):
+            # 매번 결과 추가
+            result.append(path)
+            
+            # 경로를 만들면서 DFS
+            for i in range(index, len(nums)):
+                dfs(i + 1, path, [nums[i]])
+        
+        dfs(0, [])
+        return result

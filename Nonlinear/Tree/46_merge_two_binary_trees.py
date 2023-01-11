44039c4369a7a9c9b0d4c6ed6f83d4528e854951
jinsim <simsime@korea.ac.kr>
2023-01-11 15:44:33 +0900

    46. 두 이진 트리 병합(617) - 404

---
+++ Nonlinear/Tree/46_merge_two_binary_trees.py
@@ -0,0 +1,23 @@
+""" 46. 두 이진 트리 병합(617) - 404
+두 이진 트리를 병합하라. 중복 되는 노드는 값을 합산한다.
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+
+            return node
+        else:
+            return root1 or root2
